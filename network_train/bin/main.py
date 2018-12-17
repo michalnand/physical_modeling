e9a78a3c4d6cde3d30ca5bc2b054ba6ee49aea1b
michalnand <michal.nand@gmail.com>
2018-12-17 15:32:19 +0100

    common

--- network_train/bin/main.py
+++ network_train/bin/main.py
@@ -12,8 +12,8 @@ training_dats_to_motion_tensor = pyphy.DatsToMotionTensor("training_dats.json", 
 testing_dats_to_motion_tensor = pyphy.DatsToMotionTensor("testing_dats.json", "motion_tensor.json", training_dats_to_motion_tensor.tensor())
 
 #2, create network input making class - TensorNoSpatial
-training_tensor = pyphy.TensorNoSpatial("no_spatial_tensor.json", training_dats_to_motion_tensor.tensor())
-testing_tensor  = pyphy.TensorNoSpatial("no_spatial_tensor.json", testing_dats_to_motion_tensor.tensor())
+training_tensor = pyphy.TensorSpatial("spatial_tensor.json", training_dats_to_motion_tensor.tensor())
+testing_tensor  = pyphy.TensorSpatial("spatial_tensor.json", testing_dats_to_motion_tensor.tensor())
 
 #3, create dataset
 dataset = pyphy.DatasetTrajectory(training_tensor, training_tensor)
@@ -22,16 +22,8 @@ dataset = pyphy.DatasetTrajectory(training_tensor, training_tensor)
 
 #4, run experiments, train network
 
-experiment_0 = pyphy.RegressionExperiment(dataset, "experiment_0/")
-experiment_0.run()
+experiment_12 = pyphy.RegressionExperiment(dataset, "experiment_12/")
+experiment_12.run()
 
-experiment_1 = pyphy.RegressionExperiment(dataset, "experiment_1/")
-experiment_1.run()
-
-experiment_2 = pyphy.RegressionExperiment(dataset, "experiment_2/")
-experiment_2.run()
-
-experiment_3 = pyphy.RegressionExperiment(dataset, "experiment_3/")
-experiment_3.run()
 
 print("program done")
